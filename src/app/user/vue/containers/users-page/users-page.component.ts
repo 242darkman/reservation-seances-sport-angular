8d8b4093d6a14546bf81dfae3a4a358352e41d4c
242darkman <bvouvou@gmail.com>
2023-05-29 21:13:06 +0200

    feat: ajout du smart component qui est un composant ne gérant pas la présentation des pages mais plutot la logique métier du composant

---
+++ src/app/user/vue/containers/users-page/users-page.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import filter from 'lodash/filter';
+import get from 'lodash/get';
+import { User } from '@/app/user/domain/user';
+import { UserFacadeService } from '@/app/user/application/facade/user-facade.service';
+
+@Component({
+  selector: 'app-users-page',
+  templateUrl: './users-page.component.html',
+  styleUrls: ['./users-page.component.scss'],
+})
+export class UsersPageComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userFacade: UserFacadeService) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userFacade.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+      console.log('users => ' + this.users);
+    });
+  }
+
+  addUser(user: User) {
+    if (!user) {
+      return;
+    }
+    this.userFacade.addUser(user).subscribe();
+  }
+
+  updateUser(user: User) {
+    this.userFacade.updateUser(user).subscribe();
+  }
+
+  deleteUser(user: User) {
+    this.users = filter(this.users, (u: User) => u !== user);
+    const id = get(user, 'id');
+    this.userFacade.deleteUser(id).subscribe();
+  }
+}

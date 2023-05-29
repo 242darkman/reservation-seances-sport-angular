4ae3b535414353bd8a6ae509c574cef1a4473a83
242darkman <bvouvou@gmail.com>
2023-05-29 21:08:05 +0200

    feat: chargement du module User par lazy-loading dans notre fichier principale de routing

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from '@/app/user/user-routing.module';
+import { UserService } from '@/app/user/application/services/user.service';
+import { UserFacadeService } from '@/app/user/application/facade/user-facade.service';
 @NgModule({
   declarations: [],
   imports: [CommonModule, UserRoutingModule],
+  providers: [UserService, UserFacadeService],
 })
 export class UserModule {}

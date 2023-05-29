8ee1f25078b9a13e0baa414181e20b8a37cfaf2b
242darkman <bvouvou@gmail.com>
2023-05-29 21:04:19 +0200

    feat: ajout du service de notre module qui récupère les données via l'API en mémoire et gère les opérations CRUD

---
+++ src/app/user/application/facade/user-facade.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { User } from '@/app/user/domain/user';
+import { UserService } from '@/app/user/application/services/user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserFacadeService {
+  constructor(private userService: UserService) {}
+
+  getUsers() {
+    const users = this.userService.getUsers();
+    return users;
+  }
+
+  getUser(id: number) {
+    const user = this.userService.getUser(id);
+    return user;
+  }
+
+  addUser(user: User) {
+    return this.userService.addUser(user);
+  }
+
+  updateUser(user: User) {
+    return this.userService.updateUser(user);
+  }
+
+  deleteUser(id: number) {
+    return this.userService.deleteUser(id);
+  }
+}

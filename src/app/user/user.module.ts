aaf3d77ec935359f2b58d1c3d9fbeec8c4a3dbef
242darkman <bvouvou@gmail.com>
2023-05-29 21:00:54 +0200

    feat: ajout du fichier module et du gestionnaire des sous-routes

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserRoutingModule } from '@/app/user/user-routing.module';
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, UserRoutingModule],
+})
+export class UserModule {}

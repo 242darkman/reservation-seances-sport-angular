35f68bf1e5d7463f1ddf5c9fe27fc80a147cbc56
242darkman <bvouvou@gmail.com>
2023-06-16 02:56:25 +0200

    feat: mise en place d'un guard pour vérifier les accès utilisateurs sur certaines routes

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersPageComponent } from '@/app/user/vue/containers/users-page/users-page.component';
+import { authGuard } from '@/app/auth/application/guards/auth.guard';
 
-const routes: Routes = [{ path: 'users', component: UsersPageComponent }];
+const routes: Routes = [
+  { path: 'users', component: UsersPageComponent, canActivate: [authGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

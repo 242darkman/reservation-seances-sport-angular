453e8b3bb1ffb64dff5e53d57ebdae919bd126df
242darkman <bvouvou@gmail.com>
2023-06-16 09:21:48 +0200

    feat: ajout d'un dashboard affichant les informations de l'utilisateur qu'il peut bien évidemment modifier

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersPageComponent } from '@/app/user/vue/containers/users-page/users-page.component';
+import { UserDashboardComponent } from '@/app/user/vue/components/user-dashboard/user-dashboard.component';
 import { authGuard } from '@/app/auth/application/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'users', component: UsersPageComponent, canActivate: [authGuard] },
+  { path: 'account', component: UserDashboardComponent, canActivate: [authGuard] },
 ];
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { UserRoutingModule } from '@/app/user/user-routing.module';
-import { UsersPageComponent } from '@/app/user/vue/containers/users-page/users-page.component';
-import { UserListComponent } from '@/app/user/vue/components/user-list/user-list.component';
 import { UserService } from '@/app/user/application/services/user.service';
 import { UserFacadeService } from '@/app/user/application/facade/user-facade.service';
+import { UserListComponent } from '@/app/user/vue/components/user-list/user-list.component';
+import { UsersPageComponent } from '@/app/user/vue/containers/users-page/users-page.component';
+import { UserDashboardComponent } from './vue/components/user-dashboard/user-dashboard.component';
 
 @NgModule({
-  declarations: [UsersPageComponent, UserListComponent],
-  imports: [CommonModule, UserRoutingModule, MatButtonModule, MatTableModule, MatIconModule],
+  declarations: [UsersPageComponent, UserListComponent, UserDashboardComponent],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule,
+  ],
   providers: [UserService, UserFacadeService],
 })
 export class UserModule {}

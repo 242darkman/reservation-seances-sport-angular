f26f5d4932e5eca2243653ac57767e209c844e7a
242darkman <53907600+242darkman@users.noreply.github.com>
2023-07-25 01:17:22 +0200

    fix: modification du guard de la page de listing des utilisateurs

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersPageComponent } from '@/app/user/vue/containers/users-page/users-page.component';
+
+import { NgModule } from '@angular/core';
 import { UserDashboardComponent } from '@/app/user/vue/components/user-dashboard/user-dashboard.component';
+import { UsersPageComponent } from '@/app/user/vue/containers/users-page/users-page.component';
+import { adminGuard } from '@/app/auth/application/guards/admin.guard';
 import { authGuard } from '@/app/auth/application/guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'users', component: UsersPageComponent, canActivate: [authGuard] },
+  { path: 'users', component: UsersPageComponent, canActivate: [adminGuard] },
   {
     path: 'account',
     component: UserDashboardComponent,

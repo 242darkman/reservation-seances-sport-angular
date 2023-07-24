ae172a31fd8286ed14ebd6f8eb9eccb4c10d375f
242darkman <53907600+242darkman@users.noreply.github.com>
2023-07-25 01:22:28 +0200

    docs(auth): ajout d'une documentation pour notre guard auth

--- src/app/auth/application/guards/auth.guard.ts
+++ src/app/auth/application/guards/auth.guard.ts
@@ -9,6 +9,14 @@ import { AuthService } from '@/app/auth/application/services/auth.service';
 import { inject } from '@angular/core';
 import isEmpty from 'lodash/isEmpty';
 
+/**
+ * Le garde d'authentification (auth guard) sert à protéger les routes et à vérifier si l'utilisateur est authentifié.
+ * Si l'utilisateur est authentifié et que le token est valide, l'accès à la route est autorisé.
+ * Sinon, l'utilisateur est déconnecté et redirigé vers la page de login.
+ * @param {ActivatedRouteSnapshot} route - Instantané de la route active.
+ * @param {RouterStateSnapshot} state - L'état du routeur.
+ * @returns {boolean | UrlTree} - Renvoie vrai si l'utilisateur peut accéder à la route, ou une UrlTree vers la page de login dans le cas contraire.
+ */
 export const authGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot

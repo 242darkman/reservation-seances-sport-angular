618d6bf0e5131876da7561fefeeb6c6e3c36b0c2
242darkman <53907600+242darkman@users.noreply.github.com>
2023-07-27 23:09:34 +0200

    docs: ajout d'une documentation

--- src/app/booking/application/facade/booking-facade.service.ts
+++ src/app/booking/application/facade/booking-facade.service.ts
@@ -3,6 +3,16 @@ import { BookingService } from '@/app/booking/application/service/booking.servic
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+/**
+ * @description Service de façade pour les réservations.
+ * Ce service fournit une interface simplifiée pour interagir avec le BookingService.
+ * Il permet d'obtenir des informations sur les réservations, d'insérer, de mettre à jour ou de supprimer une réservation.
+ *
+ * @example
+ * this.bookingFacadeService.getBookings();
+ *
+ * @injectable
+ */
 @Injectable({
   providedIn: 'root',
 })
@@ -11,37 +21,75 @@ export class BookingFacadeService {
 
   constructor(private bookingService: BookingService) {}
 
+  /**
+   * @description Getter pour obtenir les réservations.
+   *
+   * @returns Les réservations actuelles.
+   */
   get findBookings(): Booking[] {
     return this.bookingService.findBookings;
   }
 
+  /**
+   * @description Récupère toutes les réservations.
+   */
   getBookings() {
     this.bookingService.getBookings();
   }
 
+  /**
+   * @description Récupère les réservations d'un utilisateur.
+   *
+   * @param id L'identifiant de l'utilisateur.
+   */
   getBookingsByUserId(id: number) {
     return this.bookingService.getBookingsByUserId(id);
   }
 
+  /**
+   * @description Insère une nouvelle réservation.
+   *
+   * @param booking La nouvelle réservation.
+   * @returns Un Observable de la nouvelle réservation.
+   */
   insertBooking(booking: Booking): Observable<Booking> {
     return this.bookingService.insertBooking(booking);
   }
 
+  /**
+   * @description Met à jour une réservation existante.
+   *
+   * @param booking La réservation à mettre à jour.
+   * @param callback La fonction à exécuter après la mise à jour.
+   */
   updateBooking(booking: Booking, callback: (success: boolean) => void) {
     this.bookingService.updateBooking(booking, callback);
   }
 
+  /**
+   * @description Supprime une réservation.
+   *
+   * @param id L'identifiant de la réservation à supprimer.
+   * @param callback La fonction à exécuter après la suppression.
+   */
   deleteBooking(id: number, callback: (success: boolean) => void) {
     this.bookingService.deleteBooking(id, callback);
   }
 
+  /**
+   * @description Réserve une session.
+   *
+   * @param booking La réservation de la session.
+   * @returns Une Promesse de la nouvelle réservation.
+   */
   async bookSession(booking: Booking) {
     return await this.bookingService.bookSession(booking);
   }
 
   /**
-   * Génère un nouvel ID utilisateur via UserService.
-   * @return number L'ID généré.
+   * @description Génère un identifiant unique pour une nouvelle réservation.
+   *
+   * @returns Un identifiant numérique unique.
    */
   generateId() {
     return this.bookingService.generateId();
